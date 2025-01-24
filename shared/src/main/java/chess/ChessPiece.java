3e7fa13e89faed982138751234c8bdb95f731cce
Isaac Park <isaacjpark1@gmail.com>
2025-01-24 14:50:08 -0700

    ChessPiece.Java and Rook edits again.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -14,8 +14,8 @@ public class ChessPiece {
     private ChessPiece.PieceType pieceType;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
-    this.teamColor=pieceColor;
-    this.pieceType=type;
+        this.teamColor = pieceColor;
+        this.pieceType = type;
 
     }
 
@@ -48,11 +48,12 @@ public class ChessPiece {
         return pieceType;
     }
 
-    public String toString(){
-        char color=teamColor.toString().charAt(0);
-        char type=pieceType.toString().charAt(0);
-        return (""+color+type);
+    public String toString() {
+        char color = teamColor.toString().charAt(0);
+        char type = pieceType.toString().charAt(0);
+        return ("" + color + type);
     }
+
     /**
      * Calculates all the positions a chess piece can move to
      * Does not take into account moves that are illegal due to leaving the king in
@@ -64,28 +65,28 @@ public class ChessPiece {
         throw new RuntimeException("Not implemented");
     }
 
-    public ChessMove chessmove_direction(int x_dir,int y_dir,ChessPosition start_pos,ChessBoard board) {         //Queen,bishop,rook
+    public HashSet<ChessMove> chessmove_direction(int x_dir, int y_dir, ChessPosition start_pos, ChessBoard board) {         //Queen,bishop,rook
         int row = start_pos.getRow();
         int column = start_pos.getColumn();
         HashSet<ChessMove> piecemoves_1direction = new HashSet();
         ChessPiece piece = board.getPiece(start_pos);
         ChessGame.TeamColor piece_color = piece.getTeamColor();
-        int multiplier=0;
-        int i=multiplier*x_dir+row;
-        int j=multiplier*y_dir+column;
-        while (1<=i && i<=8 && 1<=j && j<=8) {
+        int multiplier = 0;
+        int i = multiplier * x_dir + row;
+        int j = multiplier * y_dir + column;
+        while (1 <= i && i <= 8 && 1 <= j && j <= 8) {
             ChessPiece other_piece = board.getPiece(new ChessPosition(i, j));
             if (piece != null) {
                 if (other_piece.getTeamColor() == piece_color) {
                     break;
-                }
-                 else {
-                    ChessMove move = new ChessMove(start_pos, new ChessPosition(row, i), null);
+                } else {
+                    ChessMove move = new ChessMove(start_pos, new ChessPosition(i, j), null);
                     piecemoves_1direction.add(move);
                     break;
-                }}
+                }
             }
 
         }
-
+        return piecemoves_1direction;
     }
+}
