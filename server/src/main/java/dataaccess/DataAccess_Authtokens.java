015d4516e581df878a9f1fb6d1d52f10c6e1297e
Isaac Park <isaacjpark1@gmail.com>
2025-03-01 23:59:15 -0700

    Implemented interfaces for DataAccess

--- server/src/main/java/dataaccess/DataAccess_Authtokens.java
+++ server/src/main/java/dataaccess/DataAccess_Authtokens.java
@@ -1,5 +1,14 @@
 package dataaccess;
+import Model.AuthData;
+import Model.UserData;
+import exception.ResponseException;
+
+import java.util.Collection;
 
 public interface DataAccess_Authtokens {
+    void createAuthToken(AuthData authData) throws ResponseException;
+    AuthData getAuthData(String authToken) throws ResponseException;
+    void deleteAuthData(String authToken) throws ResponseException;
+    //         Pet addPet(Pet pet) throws ResponseException;
 
 }
--- server/src/main/java/dataaccess/DataAccess_Games.java
+++ server/src/main/java/dataaccess/DataAccess_Games.java
@@ -1,4 +1,14 @@
 package dataaccess;
 
+import Model.GameData;
+import chess.ChessGame;
+import exception.ResponseException;
+
+import java.util.Collection;
+
 public interface DataAccess_Games {
+    Collection<ChessGame> listGames() throws ResponseException;
+    GameData createGame(String gameName) throws ResponseException;
+    GameData findGame(String username, int gameID) throws ResponseException;
+    void updateGame(String username, String playerColor, GameData gameData) throws ResponseException;
 }
