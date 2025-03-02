6db118c99f32cff1fb00a5628ae7f5f9d815c905
Isaac Park <isaacjpark1@gmail.com>
2025-03-01 23:31:14 -0700

    Added records for GameData, AuthData, and UserData

---
+++ shared/src/main/java/Model/AuthData.java
@@ -0,0 +1,9 @@
+package Model;
+
+import com.google.gson.Gson;
+
+public record AuthData(String authToken, String username) {
+    public String toString(){
+        return new Gson().toJson(this);
+    }
+}
--- shared/src/main/java/Model/UserData.java
+++ shared/src/main/java/Model/UserData.java
@@ -1,6 +1,6 @@
 package Model;
 import com.google.gson.*;
-public record User (String username, String password, String email){
+public record UserData(String username, String password, String email){
     public String toString(){
         return new Gson().toJson(this);
     }
