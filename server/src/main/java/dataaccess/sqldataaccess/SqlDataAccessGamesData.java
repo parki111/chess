ffcad9f49b8b9385e626626d2a9b6f09a41d65e7
Isaac Park <isaacjpark1@gmail.com>
2025-03-11 23:13:05 -0600

    SQL DataAccess Games templated methods

--- server/src/main/java/dataaccess/sqldataaccess/SqlDataAccessGamesData.java
+++ server/src/main/java/dataaccess/sqldataaccess/SqlDataAccessGamesData.java
@@ -1,4 +1,18 @@
 package dataaccess.sqldataaccess;
 
+import chess.ChessGame;
+import exception.ResponseException;
+import model.GameData;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class SqlDataAccessGamesData {
+    public int createGame(String gameName) throws ResponseException{return 1;};
+    public Collection<GameData> listGames() throws ResponseException{return new HashSet<GameData>();};
+    public GameData findGame(int gameID) throws ResponseException {return new GameData(5,"","","",new ChessGame());};
+    public Boolean updateGame(String username, String playerColor, GameData gameData) throws ResponseException{return true;};
+    //    Boolean deleteGame(String username, String playerColor, GameData gameData) throws ResponseException;
+    public void clearGames() throws ResponseException{};
 }
