54484659c96eb625441b1a235a553d60fa68c592
Isaac Park <isaacjpark1@gmail.com>
2025-03-11 23:14:24 -0600

    SQL DataAccess AuthData templated methods

--- server/src/main/java/dataaccess/sqldataaccess/SqlDataAccessAuthData.java
+++ server/src/main/java/dataaccess/sqldataaccess/SqlDataAccessAuthData.java
@@ -1,4 +1,11 @@
 package dataaccess.sqldataaccess;
 
+import exception.ResponseException;
+import model.AuthData;
+
 public class SqlDataAccessAuthData {
+    public AuthData addAuthData(AuthData authData) throws ResponseException{return new AuthData("","");};
+    public AuthData getAuthData(String authToken) throws ResponseException{return new AuthData("","");};
+    public Boolean deleteAuthData(String authToken) throws ResponseException{return true;};
+    public void clearAuthData() throws ResponseException{};
 }
