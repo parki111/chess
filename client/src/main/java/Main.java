9681bce6de48f2b46799f8985fd8a7f65dc6cd98
Isaac Park <isaacjpark1@gmail.com>
2025-04-10 19:58:15 -0600

    WebsocketFacade/Client progress

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -10,9 +10,7 @@ public class Main {
     public static void main(String[] args) throws ResponseException {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
-        ChessGame chessGame = new ChessGame();
-        chessGame.getBoard().addPiece(new ChessPosition(5,5),new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN));
-        new ChessBoardUI(ChessGame.TeamColor.WHITE,chessGame,new ChessPosition(5,5)).chessBoardWhite();
+
         Repl repl = new Repl ("http://localhost:8080");
         repl.run();
     }
